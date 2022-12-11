0c43c19234379e1adf08bf7149672a965fb4ba45
Utkarsh Singh <utkarshshail005@protonmail.com>
2022-12-12 03:58:05 +0530

    Hedera Sentient Marketplace Integration

--- sales.js
+++ sales.js
@@ -12,6 +12,7 @@ const discord_api=process.env.DISCORD_API
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
 var mainZuseAccount = `0.0.703235`
+var mainSentientAccount = `0.0.1223480`
 
 function toTimestamp(strDate){
     var datum = Date.parse(strDate);
@@ -126,6 +127,8 @@ client.api.applications(client.user.id).commands.post({data:
 
 var timeStampZuse = Math.floor(Date.now() / 1000)
 var timeStampHashguild = Math.floor(Date.now() / 1000)
+var timeStampSentient = Math.floor(Date.now() / 1000)
+
 
 try{
 
@@ -228,22 +231,22 @@ for(var tx of transactions){
 
     // TO GET HBAR PRICE
 
-    while(true){
-        try{
+    // while(true){
+    //     try{
 
-    var priceData = await CoinGeckoClient.simple.price({
-        ids: ['hedera-hashgraph'],
-        vs_currencies: ['usd'],
-    }); 
+    // var priceData = await CoinGeckoClient.simple.price({
+    //     ids: ['hedera-hashgraph'],
+    //     vs_currencies: ['usd'],
+    // }); 
     
-    var coinPrice = (priceData['data']['hedera-hashgraph']['usd']*value).toFixed(2)
-    break
+    // var coinPrice = (priceData['data']['hedera-hashgraph']['usd']*value).toFixed(2)
+    // break
 
-        }catch(e){
-            console.log(e)
-            console.log(`Retrying Coingecko API`)
-        }
-    }
+    //     }catch(e){
+    //         console.log(e)
+    //         console.log(`Retrying Coingecko API`)
+    //     }
+    // }
 
     var nftImage = `https://zmedia.b-cdn.net/file/z35e-awg5/media/${nftTokenId}/${nftSerial}`
 
@@ -266,7 +269,7 @@ for(var tx of transactions){
     .setColor('#808080')
     .setAuthor({ name: 'Zuse Sales', iconURL: 'https://zuse.market/img/zuse_logo.2031c4b5.png'})
     .setTitle(`${nftName} ${nftSerial} SOLD!`)
-    .setDescription(`\n**__Collection__**\n[${nftName}](https://zuse.market/collection/${nftTokenId})\n\n**__Price__**\n${value}ℏ ($${coinPrice})\n\n**__Buyer__**\n[${buyer}](https://hashscan.io/#/mainnet/account/${buyer})\n\n**__Seller__**\n[${seller}](https://hashscan.io/#/mainnet/account/${seller})\n`)
+    .setDescription(`\n**__Collection__**\n[${nftName}](https://zuse.market/collection/${nftTokenId})\n\n**__Price__**\n${value}ℏ \n\n**__Buyer__**\n[${buyer}](https://hashscan.io/#/mainnet/account/${buyer})\n\n**__Seller__**\n[${seller}](https://hashscan.io/#/mainnet/account/${seller})\n`)
     .setImage(nftImage)
     .setURL(`https://hederaexplorer.io/search-details/transaction/${txID}`)
     .setTimestamp(new Date())
@@ -345,6 +348,244 @@ console.log(`Retrying API request (1)`)
 
 if (transactions.length!=0){
     timeStampZuse = parseInt(transactions[0]['consensus_timestamp'])+1
+    try{
+
+        var GUILD_ID = client.guilds.cache.map(guild => guild.id);
+        client.user.setActivity(`Sales On ${GUILD_ID.length} Servers`, { type: 'WATCHING' });
+            
+        }catch(e){console.log(`Error in updating status!`)}
+}
+
+
+// FOR SENTIENT MARKETPLACE
+
+while(true){
+    try{
+
+// To get new transactions in interval of 1 second
+
+var url=`https://mainnet-public.mirrornode.hedera.com/api/v1/transactions?account.id=${mainSentientAccount}&limit=100&order=desc&timestamp=gt%3A${timeStampSentient}&transactiontype=cryptotransfer&result=success`
+
+
+var opts = {
+    headers:{
+        'accept': 'application/json'
+    }
+}
+
+var response = await web_call(url,opts)
+
+
+var transactions = (response['transactions'])
+
+
+for(var tx of transactions){
+
+    var txID = tx['transaction_id']
+
+    while(true){
+        try{
+
+    // To get each transactions info
+
+    var url=`https://mainnet-public.mirrornode.hedera.com/api/v1/transactions/${txID}?nonce=0`
+
+    var opts = {
+        headers:{
+            'accept': 'application/json'
+        }
+    }
+    
+    var response = await web_call(url,opts)
+    break
+
+        }catch(e){console.log(e)
+        console.log(`Retrying API request (2)`)
+        }
+    }
+    
+    var mainTx = (response['transactions'][0]['nft_transfers'])
+    var mainTransfers = (response['transactions'][0]['transfers'])
+
+    for (nftTx of mainTx){
+        var nftTokenId = nftTx['token_id']
+        var nftSerial = nftTx['serial_number'] 
+        var buyer = nftTx['receiver_account_id']
+        var seller = nftTx['sender_account_id']
+    }
+
+    for(transfers of mainTransfers){
+        if (transfers['account']==buyer){
+            var value = Math.abs(parseInt(transfers['amount']))/10**8
+        }
+    }
+
+
+    while(true){
+        try{
+
+    // To get NFT NAME from TOKEN ID 
+
+    var url=`https://mainnet-public.mirrornode.hedera.com/api/v1/tokens/${nftTokenId}`
+
+    var opts = {
+        headers:{
+            'accept': 'application/json'
+        }
+    }
+    
+    var response = await web_call(url,opts)
+    break
+
+        }catch(e){console.log(e)
+        console.log(`Retrying API request (3)`)
+        }
+    }
+
+    var nftName = response['name']
+
+    // TO GET NFT IMAGE 
+
+    var sentientImgName=(nftName.toLocaleLowerCase()).replace(' ','-')
+
+    var url=`https://hederasentientbackend.azurewebsites.net/nftexplorer-nft`
+
+    var opts = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            "friendlyurl": sentientImgName,
+            "serialId": nftSerial,
+            "ismarket": false
+        })
+    }
+    
+    
+    var response = await web_call(url,opts)
+
+    var nftImage = response['response']['imageCDNURL']
+
+    // TO GET HBAR PRICE
+
+    // while(true){
+    //     try{
+
+    // var priceData = await CoinGeckoClient.simple.price({
+    //     ids: ['hedera-hashgraph'],
+    //     vs_currencies: ['usd'],
+    // }); 
+    
+    // var coinPrice = (priceData['data']['hedera-hashgraph']['usd']*value).toFixed(2)
+    // break
+
+    //     }catch(e){
+    //         console.log(e)
+    //         console.log(`Retrying Coingecko API`)
+    //     }
+    // }
+
+
+
+
+    console.log(
+    ` 
+    Name -> ${nftName}
+    Buyer -> ${buyer}
+    Seller -> ${seller}
+    Nft Contract ->  ${nftTokenId}
+    Token ID ->  ${nftSerial}
+    Value -> ${value}
+    Image -> ${nftImage}
+    Tx ID -> ${txID}
+    `)
+    
+
+    const exampleEmbed = new MessageEmbed()
+    .setColor('#808080')
+    .setAuthor({ name: 'Sentient Sales', iconURL: 'https://hederasentient.com/assets/images/logo-dark.png'})
+    .setTitle(`${nftName} ${nftSerial} SOLD!`)
+    .setDescription(`\n**__Collection__**\n[${nftName}](https://hederasentient.com/nft-explorer/${sentientImgName})\n\n**__Price__**\n${value}ℏ \n\n**__Buyer__**\n[${buyer}](https://hashscan.io/#/mainnet/account/${buyer})\n\n**__Seller__**\n[${seller}](https://hashscan.io/#/mainnet/account/${seller})\n`)
+    .setImage(nftImage)
+    .setURL(`https://hederaexplorer.io/search-details/transaction/${txID}`)
+    .setTimestamp(new Date())
+    .setFooter({ text: 'Made by 0xAnon#0602'});
+
+
+    var db = new sqlite3.Database('servers.db');
+
+    db.serialize(function() {
+
+        var sql = 'SELECT * FROM users';
+
+        db.all(sql, [], async(err, rows) => {
+
+        if (err) return console.error(err.message);
+
+        rows.forEach((row) =>{
+            channelContract=[]
+            var channelContractTemp=row['contracts'].split(','); 
+            channelContractTemp.forEach(element => {
+                if (element!=''){
+                    channelContract.push(element)
+                }
+            });
+
+
+            if (row['allCollection']==true){
+                try{
+                var channelInfo = client.channels.cache.get(row['channelID'])
+                client.channels.cache.get(row['channelID']).send({ embeds: [exampleEmbed] }).catch(e =>{
+                   console.log(`Error in sending to ${channelInfo.name} channel on ${channelInfo.guild.name} server`)
+               })
+                    }catch(e){
+                        if(channelInfo==undefined){console.log(`Error in sending to unknown Channel`)}
+                        else{
+                        console.log(`Error in sending to ${channelInfo.name} channel on ${channelInfo.guild.name} server`)
+                        }
+                    }
+
+            }else{
+            if (channelContract.length!=0){
+                if (channelContract.includes(nftTokenId)){
+                    try{
+                    var channelInfo = client.channels.cache.get(row['channelID'])
+                    client.channels.cache.get(row['channelID']).send({ embeds: [exampleEmbed] }).catch(e =>{
+                       console.log(`Error in sending to ${channelInfo.name} channel on ${channelInfo.guild.name} server`)
+                   })
+                        }catch(e){
+                            if(channelInfo==undefined){console.log(`Error in sending to unknown Channel`)}
+                            else{
+                            console.log(`Error in sending to ${channelInfo.name} channel on ${channelInfo.guild.name} server`)
+                            }
+
+                        }
+                }
+         }
+
+            }
+
+        });
+
+        });
+    })
+
+     
+    
+}
+
+break
+
+}catch(e){console.log(e)
+console.log(`Retrying API request (1)`)
+}
+
+}
+
+
+if (transactions.length!=0){
+    timeStampSentient = parseInt(transactions[0]['consensus_timestamp'])+1
     try{
 
         var GUILD_ID = client.guilds.cache.map(guild => guild.id);
@@ -474,22 +715,22 @@ while(true){
     
                     var nftName = response['name']
          
-                    while(true){
-                    try{
+                    // while(true){
+                    // try{
 
-                    var priceData = await CoinGeckoClient.simple.price({
-                        ids: ['hedera-hashgraph'],
-                        vs_currencies: ['usd'],
-                    }); 
+                    // var priceData = await CoinGeckoClient.simple.price({
+                    //     ids: ['hedera-hashgraph'],
+                    //     vs_currencies: ['usd'],
+                    // }); 
                     
-                    var coinPrice = (priceData['data']['hedera-hashgraph']['usd']*value).toFixed(2)
-                    break
+                    // var coinPrice = (priceData['data']['hedera-hashgraph']['usd']*value).toFixed(2)
+                    // break
 
-                    }catch(e){
-                        console.log(e)
-                        console.log(`Retrying Coingecko API`)                        
-                        }
-                    }   
+                    // }catch(e){
+                    //     console.log(e)
+                    //     console.log(`Retrying Coingecko API`)                        
+                    //     }
+                    // }   
 
 
     
@@ -510,7 +751,7 @@ while(true){
                     .setColor('#808080')
                     .setAuthor({ name: 'HashGuild Sales', iconURL: 'https://www.trst-nft.com/wp-content/uploads/2022/05/HG-sponsor-logo.png'})
                     .setTitle(`${nftName} ${nftSerial} SOLD!`)
-                    .setDescription(`\n**__Collection__**\n[${nftName}](https://hashguild.xyz/collection/${nftTokenId})\n\n**__Price__**\n${value} HBAR ($${coinPrice})\n\n**__Buyer__**\n[${buyer}](https://hashscan.io/#/mainnet/account/${buyer})\n\n**__Seller__**\n[${seller}](https://hashscan.io/#/mainnet/account/${seller})\n`)
+                    .setDescription(`\n**__Collection__**\n[${nftName}](https://hashguild.xyz/collection/${nftTokenId})\n\n**__Price__**\n${value} HBAR \n\n**__Buyer__**\n[${buyer}](https://hashscan.io/#/mainnet/account/${buyer})\n\n**__Seller__**\n[${seller}](https://hashscan.io/#/mainnet/account/${seller})\n`)
                     .setImage(nftImage)
                     .setURL(`https://hashguild.xyz/assets/${nftTokenId}/${nftSerial}`)
                     .setTimestamp(new Date())
