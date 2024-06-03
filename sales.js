897065f1aa44dbd7aac783e852a5e9c42e4fc999
0xAnon <utkarshshail005@protonmail.com>
2024-06-04 02:57:49 +0530

    Small Update

--- sales.js
+++ sales.js
@@ -25,7 +25,7 @@ function getSentxToken(_paymentTokenType){
 
     if(_paymentTokenType == 1) return `ℏ`
     else if(_paymentTokenType == 24) return ` $STEAM`
-    else return ` N/A`
+    else return null
 
 }
 
@@ -148,15 +148,20 @@ while(true){
 
         var opts = {
             method: "POST",
-            headers:{
-                'accept': 'application/json'
-            },       
+            headers: {
+                'accept': 'application/json, text/plain, */*',
+                'accept-language': 'en-GB,en;q=0.5',
+                'content-type': 'application/json',
+            },
+            referrer: 'https://sentx.io/',
             body: JSON.stringify({
                 "page": 1,
+                "amount": 50,
                 "activityFilterArray": [1],
                 "allHTSTokens": true,
-            })
-        }
+                "minPrice": 0
+            }),
+        };
 
         var response = await web_call(url,opts)
         var transactions = (response['response'])
@@ -181,53 +186,57 @@ while(true){
                 var txID = tx['saleTransactionId']
                 var tokenName = getSentxToken(tx['paymentTokenId'])
 
-                try{
-                    const extension = nftImage.slice(-3)
-                    if(extension=="jpg"){
-                        nftImage = nftImage.replace("w32","w500")
-                    }
-                }catch(e){
-                    console.log(e)
-                    console.log('Something Went Wrong in SENTX Image')
-                    nftImage = `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvUio5S_e5H0Yo2B0dBsEC09oLUDIOORQM4w&usqp=CAU`
-                }
+                if(tokenName!=null){
 
-                console.log(
-                ` 
-                Name -> ${nftName}
-                Buyer -> ${buyer}
-                Seller -> ${seller}
-                Nft Contract ->  ${nftTokenId}
-                Token ID ->  ${nftSerial}
-                Value -> ${value}
-                Image -> ${nftImage}
-                Tx ID -> ${txID}
-                `)
-                
-
-                const exampleEmbed = new MessageEmbed()
-                .setColor('#808080')
-                .setAuthor({ name: 'SentX Sales', iconURL: 'https://blob.sentx.io/media/web/logo-sm-notrans.png?width=128'})
-                .setTitle(`${nftName} ${nftSerial} SOLD!`)
-                .setDescription(`\n**__Collection__**\n[${nftName}](https://sentx.io/nft-marketplace/${nftTokenId})\n\n**__Price__**\n${value}${tokenName} \n\n**__Buyer__**\n[${buyer}](https://hashscan.io/mainnet/account/${buyer})\n\n**__Seller__**\n[${seller}](https://hashscan.io/mainnet/account/${seller})\n`)
-                .setImage(nftImage)
-                .setURL(`https://hederaexplorer.io/search-details/tran7576saction/${txID}`)
-                .setTimestamp(new Date())
-                .setFooter({ text: 'Made by 0xAnon'});
-
-
-                var allMatching = await getContracts(nftTokenId)
-
-                for(const row of allMatching){
                     try{
-                        var channelInfo = client.channels.cache.get(row['channelID'])
-                        client.channels.cache.get(row['channelID']).send({ embeds: [exampleEmbed] }).catch(e =>{
-                        console.log(`Error in sending to server`)
-                    })
+                        const extension = nftImage.slice(-3)
+                        if(extension=="jpg"){
+                            nftImage = nftImage.replace("w32","w500")
+                        }
                     }catch(e){
-                        if(channelInfo==undefined){console.log(`Error in sending to unknown Channel`)}
+                        console.log(e)
+                        console.log('Something Went Wrong in SENTX Image')
+                        nftImage = `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvUio5S_e5H0Yo2B0dBsEC09oLUDIOORQM4w&usqp=CAU`
                     }
 
+                    console.log(
+                    ` 
+                    Name -> ${nftName}
+                    Buyer -> ${buyer}
+                    Seller -> ${seller}
+                    Nft Contract ->  ${nftTokenId}
+                    Token ID ->  ${nftSerial}
+                    Value -> ${value}
+                    Image -> ${nftImage}
+                    Tx ID -> ${txID}
+                    `)
+                    
+
+                    const exampleEmbed = new MessageEmbed()
+                    .setColor('#808080')
+                    .setAuthor({ name: 'SentX Sales', iconURL: 'https://blob.sentx.io/media/web/logo-sm-notrans.png?width=128'})
+                    .setTitle(`${nftName} ${nftSerial} SOLD!`)
+                    .setDescription(`\n**__Collection__**\n[${nftName}](https://sentx.io/nft-marketplace/${nftTokenId})\n\n**__Price__**\n${value}${tokenName} \n\n**__Buyer__**\n[${buyer}](https://hashscan.io/mainnet/account/${buyer})\n\n**__Seller__**\n[${seller}](https://hashscan.io/mainnet/account/${seller})\n`)
+                    .setImage(nftImage)
+                    .setURL(`https://hederaexplorer.io/search-details/tran7576saction/${txID}`)
+                    .setTimestamp(new Date())
+                    .setFooter({ text: 'Made by 0xAnon'});
+
+
+                    var allMatching = await getContracts(nftTokenId)
+
+                    for(const row of allMatching){
+                        try{
+                            var channelInfo = client.channels.cache.get(row['channelID'])
+                            client.channels.cache.get(row['channelID']).send({ embeds: [exampleEmbed] }).catch(e =>{
+                            console.log(`Error in sending to server`)
+                        })
+                        }catch(e){
+                            if(channelInfo==undefined){console.log(`Error in sending to unknown Channel`)}
+                        }
+
+                    }
+                
                 }
 
                 tempSentientTimestamp=txTimestamp
@@ -248,7 +257,7 @@ while(true){
 
     try{
 
-        let url=`https://labs.kabila.app/api/marketplace/analytics/activity?skip=0&limit=25&timeRange=2m&format=JSONEachRow&fields=tokenId%2CcollectionName%2CserialNumber%2Cname%2CimageCid%2CactivityType%2CsubactivityType%2CverificationType%2Cprice%2Ccurrency%2CbuyerId%2CsellerId%2CcreatedAt&activityType=SALE`
+        let url=`https://labs.kabila.app/api/marketplace/analytics/activity?skip=0&limit=25&timeRange=2m&format=JSONEachRow&fields=tokenId%2CcollectionName%2CserialNumber%2Cname%2CimageCid%2CactivityType%2CsubactivityType%2CverificationType%2Cprice%2Ccurrency%2CbuyerId%2CsellerId%2Crank%2CcreatedAt&activityType=SALE`
 
         let opts = {
             method: "GET",
